ebab11f61714acdcba19990d098b31030ddd9c54
gabeesms <gabriellemacieel@gmaill.com>
2020-07-14 14:53:40 -0400

    initial commit

--- exer12.php
+++ exer12.php
@@ -7,8 +7,37 @@
     Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (A-álcool,
     G-gasolina) e imprima o valor a ser pago pelo cliente.
     Considere que o preço do litro da gasolina é ​R$ 2,99​ e o preço do litro do álcool é ​R$ 2,19​.*/
-
     
+    $qtdalcool = 21;
+    $qtdgasolina = 30;
+
+    if($qtdalcool <= 20){
+        $alcool = 2.19 - ((2.19*3)/100); 
+        $totalalcool = $qtdalcool * $alcool;
+        echo "Valor desconto alcool: $alcool  <br />\n";
+        echo "Valor total alcool: $totalalcool  <br />\n";
+    }else{
+        $alcool = 2.19 - ((2.19*5)/100);
+        $totalalcool = $qtdalcool * $alcool;
+        echo "Valor desconto alcool: $alcool  <br />\n";
+        echo "Valor total alcool: $totalalcool  <br />\n";
+    }
+
+    if($qtdgasolina <= 20){
+        $gasolina = 2.99 - ((2.99*4)/100); 
+        $totalgasolina = $qtdgasolina * $gasolina;
+        echo "Valor desconto gasolina: $gasolina <br />\n";
+        echo "Valor total gasolina: $totalgasolina  <br />\n";
+    }else{
+        $gasolina = 2.99 - ((2.19*6)/100);
+        $totalgasolina = $qtdgasolina * $gasolina;
+        echo "Valor desconto gasolina: $gasolina  <br />\n";
+        echo "Valor total gasolina: $totalgasolina  <br />\n";
+    }
+
+
+
+
     
 
     ?>
